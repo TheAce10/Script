76d2bf9cdbb54b643b5a17929884da8fd55fcc17
Elikem <99412803+TheAce10@users.noreply.github.com>
2024-06-29 03:32:02 +0000

    Update Functions.py

--- package1/Functions.py
+++ package1/Functions.py
@@ -1,48 +1,48 @@
-# def swap():
-#     a=input("Enter a first number \n")
-#     b=input("Enter a second number \n")
-#     a=int(a)+int(b)
-#     b=int(a)-int(b)
-#     a=int(a)-int(b)
-#     print(f"a is now {a} and b is now {b}")
+def swap():
+    a=input("Enter a first number \n")
+    b=input("Enter a second number \n")
+    a=int(a)+int(b)
+    b=int(a)-int(b)
+    a=int(a)-int(b)
+    print(f"a is now {a} and b is now {b}")
 # swap()
-# def fact(num):
-#     b = num
-#     while num > 0:
-#         num= int(num)*(int(num)-1)
-#     print(f"{b}! is {num}")
+def fact(num):
+    b = num
+    while num > 0:
+        num= int(num)*(int(num)-1)
+    print(f"{b}! is {num}")
 
-# #A function to find factorial
-# def fact(ind):
-#     num=ind
-#     result=num
-#     while num > 1:
-#         result=result * (num - 1)
-#         num = num - 1
-#     print(f"\n{ind}! is",result)
+#A function to find factorial
+def fact(ind):
+    num=ind
+    result=num
+    while num > 1:
+        result=result * (num - 1)
+        num = num - 1
+    print(f"\n{ind}! is",result)
 
 # num=int(input("Enter Factorial:\n"))
 # fact(num)
 
-# def fact(ind):
-#     num=ind
-#     result=num
-#     while num > 1:
-#         result=result * (num - 1)
-#         num = num - 1
-#     print(f"\n{ind}! is",result)
+def fact(ind):
+    num=ind
+    result=num
+    while num > 1:
+        result=result * (num - 1)
+        num = num - 1
+    print(f"\n{ind}! is",result)
 
 # fact(5)
 
-# def sumfact(x):
-#     term=x
-#     result=x
-#     while x>0:
-#         result= result + (x-1)
-#         x = x - 1
-#     print(f'Sum of integers from 0 to {term} is {result}')
+def sumfact(x):
+    term=x
+    result=x
+    while x>0:
+        result= result + (x-1)
+        x = x - 1
+    print(f'Sum of integers from 0 to {term} is {result}')
 
-# sumfact(100)
+sumfact(100)
 
 print(len(rf"123\\"))
-#  + len("\\\\////"))
+ + len("\\\\////"))
